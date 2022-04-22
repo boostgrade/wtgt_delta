0986d93248a4285641d5c7794419d3484366621a
Gleb Perveev <sanlovty@gmail.com>
2022-04-23 02:59:55 +0300

    feat(services): handling for new status-codes in server-error-mapper
    - added new statusCodes
    - added different handling based on statusCodes

--- lib/src/core/services/exceptions/server/server_error_mapper.dart
+++ lib/src/core/services/exceptions/server/server_error_mapper.dart
@@ -1,22 +1,40 @@
+import 'dart:convert';
 import 'package:dio/dio.dart';
-
-import 'server_exceptions.dart';
+import 'package:where_to_go_today/src/core/services/exceptions/server/server_exceptions.dart';
 
 /// Сущность, которая преобразует ошибки сервера в ошибки приложенния
 /// для дальнейшей обработки.
 class ServerErrorMapper {
   /// Для обработки остальных серверных ошибок
   /// необходимо задать новые целочисленные константы, соотвтетсвующие кодам.
+  static const int _badRequest = 400;
+  static const int _unauthorized = 401;
   static const int _notFound = 404;
 
   static Exception fromDioError(DioError error) {
     final statusCode = error.response?.statusCode!;
-    
+
     /// Для обработки остальных серверных ошибок
     /// нужно написать дополнительные блоки в условном выражении,
     /// предварительно создав классы исключений.
-    if (statusCode == _notFound) {
-      return NotFoundException();
+    switch (statusCode) {
+      case _notFound:
+        return NotFoundException();
+      case _unauthorized:
+        return UnauthorizedException();
+      case _badRequest:
+        if (error.response != null) {
+          final code = json.decode(error.response.toString())['code'] as int;
+
+          switch (code) {
+            case 101:
+              return ImageSoLargeException();
+            default:
+              break;
+          }
+        }
+
+        break;
     }
 
     /// Возвращается по-умолчанию для остальных ошибок
