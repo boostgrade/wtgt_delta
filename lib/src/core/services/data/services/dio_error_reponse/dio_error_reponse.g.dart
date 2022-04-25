0c86fb0956e7267dc79ae612a8706664b390c60e
Gleb Perveev <sanlovty@gmail.com>
2022-04-25 19:01:18 +0300

    feat(services-data): added DioErrorResponse model

---
+++ lib/src/core/services/data/services/dio_error_reponse/dio_error_reponse.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio_error_reponse.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_DioErrorResponse _$$_DioErrorResponseFromJson(Map<String, dynamic> json) =>
+    _$_DioErrorResponse(
+      code: json['code'] as int?,
+      message: json['message'] as String?,
+    );
+
+Map<String, dynamic> _$$_DioErrorResponseToJson(_$_DioErrorResponse instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'message': instance.message,
+    };
