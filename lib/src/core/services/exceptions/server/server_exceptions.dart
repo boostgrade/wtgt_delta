55876e8f41543dd344cff1a87fda4a8eb479d7f5
Gleb Perveev <sanlovty@gmail.com>
2022-04-25 19:04:42 +0300

    feat(exceptions): added general class for ServerExceptions
    Added general class (CustomServerException) with factory from DioErrorResponse. (factory returns CustomServerException children depending on it's code)

--- lib/src/core/services/exceptions/server/server_exceptions.dart
+++ lib/src/core/services/exceptions/server/server_exceptions.dart
@@ -1,17 +1,36 @@
+import 'package:where_to_go_today/src/core/services/data/services/dio_error_reponse/dio_error_reponse.dart';
+
 /// Для преобразования ошибок сервера в ошибки приложенния,
 /// необходимо создать соответсвующие классы,
-/// расширяющие класс [Exception]
+/// расширяющие класс [CustomServerException]
 /// с названиями, соответсвующими конкретной ошибке.
 
+/// Общий класс ошибок, с фабричным методом обработки ошибок по коду в response'e
+abstract class CustomServerException implements Exception {
+  factory CustomServerException(DioErrorResponse response) {
+    if (response.code == null) {
+      return ServerErrorException();
+    }
+
+    switch (response.code) {
+      case 101:
+        return ImageSoLargeException();
+      default:
+        break;
+    }
+
+    return ServerErrorException();
+  }
+}
+
 /// Серверная ошибка по-умолчанию
-class ServerErrorException implements Exception {}
+class ServerErrorException implements CustomServerException {}
 
 /// Ошибка 404
-class NotFoundException implements Exception {}
+class NotFoundException implements CustomServerException {}
 
 /// Ошибка 400 с кодом code == 101
-class ImageSoLargeException implements Exception {}
+class ImageSoLargeException implements CustomServerException {}
 
 /// Ошибка при протухшем токене
-class UnauthorizedException implements Exception {}
-
+class UnauthorizedException implements CustomServerException {}
