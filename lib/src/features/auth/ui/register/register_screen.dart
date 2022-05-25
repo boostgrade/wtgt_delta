fee1851d502faa3b3833ec5873085a8a25d4997b
tvkaraev <timur.karev@eyevox.ru>
2022-05-26 02:29:10 +0300

    feat(registration_screen): валидация экрана

--- lib/src/features/auth/ui/register/register_screen.dart
+++ lib/src/features/auth/ui/register/register_screen.dart
@@ -13,6 +13,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _key = GlobalKey<FormState>();
   String? _date;
   bool _checkBoxValue = false;
 
@@ -24,72 +25,90 @@ class _RegisterScreenState extends State<RegisterScreen> {
         right: 24.0,
         top: 50.0,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: WtgtForm(
-              onChanged: (_) {},
-              labelText: AppLocalizations.of(context)!.textFieldHintName,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: WtgtForm(
-              onChanged: (_) {},
-              labelText: AppLocalizations.of(context)!.textFieldHintLastName,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: WtgtForm(
-              onChanged: (_) {},
-              labelText: AppLocalizations.of(context)!.textFieldHintEmail,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: WtgtForm(
-              key: _date == null ? null : Key(_date!),
-              value: _date,
-              onChanged: (_) {},
-              labelText: AppLocalizations.of(context)!.textFieldHintBirthdate,
-              suffixIcon: InkWell(
-                onTap: _setDate,
-                child: const Icon(Icons.calendar_today_outlined),
+      child: Form(
+        key: _key,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: WtgtForm(
+                defaultValue: true,
+                validatorPattern: r'^[а-я-А-Я]*$',
+                onChanged: (_) {
+                  //TODO(any): обработать нажатие на кнопку
+                },
+                labelText: AppLocalizations.of(context)!.textFieldHintName,
               ),
             ),
-          ),
-          const Spacer(),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              WtgtCheckBox(
-                value: _checkBoxValue,
-                onChange: _checkboxOnChange,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: WtgtForm(
+                defaultValue: true,
+                validatorPattern: r'^[а-я-А-Я]*$',
+                onChanged: (_) {
+                  //TODO(any): обработать нажатие на кнопку
+                },
+                labelText: AppLocalizations.of(context)!.textFieldHintLastName,
               ),
-              const SizedBox(
-                width: 24.0,
-              ),
-              Expanded(
-                child: Text(AppLocalizations.of(context)!.acceptUserAgreement),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 24.0,
             ),
-            child: WtgtButton(
-              type: ButtonType.active,
-              label: AppLocalizations.of(context)!.buttonRegistration,
-              onPressed: () {},
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: WtgtForm(
+                defaultValue: true,
+                validatorPattern:
+                    r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$",
+                onChanged: (_) {
+                  //TODO(any): обработать нажатие на кнопку
+                },
+                labelText: AppLocalizations.of(context)!.textFieldHintEmail,
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: WtgtForm(
+                key: _date == null ? null : Key(_date!),
+                value: _date,
+                onChanged: (_) {
+                  //TODO(any): обработать нажатие на кнопку
+                },
+                labelText: AppLocalizations.of(context)!.textFieldHintBirthdate,
+                suffixIcon: InkWell(
+                  onTap: _setDate,
+                  child: const Icon(Icons.calendar_today_outlined),
+                ),
+              ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                WtgtCheckBox(
+                  value: _checkBoxValue,
+                  onChange: _checkboxOnChange,
+                ),
+                const SizedBox(
+                  width: 24.0,
+                ),
+                Expanded(
+                  child: Text(AppLocalizations.of(context)!.acceptUserAgreement),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 24.0,
+              ),
+              child: WtgtButton(
+                type: ButtonType.active,
+                label: AppLocalizations.of(context)!.buttonRegistration,
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/ui/uikit/wtgt_form.dart
+++ lib/src/ui/uikit/wtgt_form.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:where_to_go_today/src/core/ui/res/typography/typography.dart';
 
 class WtgtForm extends StatelessWidget {
   final Function(String) onChanged;
-  final TextEditingController? controller;
   final String? labelText;
   final Widget? suffixIcon;
   final String? value;
+  final String? validatorPattern;
+  final bool defaultValue;
 
   const WtgtForm({
     Key? key,
     required this.onChanged,
-    this.controller,
+    this.validatorPattern,
+    this.defaultValue = false,
     this.value,
     this.labelText,
     this.suffixIcon,
@@ -19,17 +22,34 @@ class WtgtForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 52.0,
-      child: TextFormField(
-        initialValue: value,
-        onChanged: onChanged,
-        style: AppTypography.subtitle1,
-        decoration: InputDecoration(
-          labelText: labelText,
-          suffixIcon: suffixIcon,
-        ),
+    return TextFormField(
+      initialValue: value,
+      onChanged: onChanged,
+      style: AppTypography.subtitle1,
+      decoration: InputDecoration(
+        labelText: labelText,
+        suffixIcon: suffixIcon,
       ),
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => _validator(context, value),
     );
+  }
+
+  String? _validator(BuildContext context, String? value) {
+    if (defaultValue && (value == null || value.isEmpty)) {
+      return AppLocalizations.of(context)!.textFieldErrorEmpty;
+    }
+    if ((validatorPattern ?? '').isEmpty) {
+      return null;
+    }
+    final regExp = RegExp(
+      validatorPattern!,
+      caseSensitive: false,
+    );
+    if (regExp.hasMatch(value!)) {
+      return null;
+    }
+
+    return AppLocalizations.of(context)?.textFieldErrorNotMatch;
   }
 }
