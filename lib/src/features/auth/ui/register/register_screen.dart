80817e872371f4c60ec8d1c4daa1ec940875238a
tvkaraev <timur.karev@eyevox.ru>
2022-05-26 16:15:41 +0300

    feat(registration_screen): фиксы статического анализатора

--- lib/src/features/auth/ui/register/register_screen.dart
+++ lib/src/features/auth/ui/register/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:where_to_go_today/src/core/constants/text_field_regexp.dart';
 import 'package:where_to_go_today/src/core/ui/res/typography/typography.dart';
 import 'package:where_to_go_today/src/core/utils/date_utils.dart' as utils;
 import 'package:where_to_go_today/src/ui/uikit/wtgt_button.dart';
@@ -35,7 +36,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: WtgtForm(
                 defaultValue: true,
-                validatorPattern: r'^[а-я-А-Я]*$',
+                validatorPattern: TextFieldRegExp.name,
                 onChanged: (_) {
                   //TODO(any): обработать нажатие на кнопку
                 },
@@ -46,7 +47,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: WtgtForm(
                 defaultValue: true,
-                validatorPattern: r'^[а-я-А-Я]*$',
+                validatorPattern: TextFieldRegExp.name,
                 onChanged: (_) {
                   //TODO(any): обработать нажатие на кнопку
                 },
@@ -57,8 +58,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: WtgtForm(
                 defaultValue: true,
-                validatorPattern:
-                    r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$",
+                validatorPattern: TextFieldRegExp.email,
                 onChanged: (_) {
                   //TODO(any): обработать нажатие на кнопку
                 },
@@ -71,7 +71,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 key: _date == null ? null : Key(_date!),
                 value: _date,
                 defaultValue: true,
-                validatorPattern: r'^[0-3]{0,1}[0-9].[0-1]{0,1}[0-9].[0-2][0,9][0-9]{2}$',
+                validatorPattern: TextFieldRegExp.date,
                 onChanged: (_) {
                   //TODO(any): обработать нажатие на кнопку
                 },
